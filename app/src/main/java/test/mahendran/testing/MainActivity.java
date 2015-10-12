09bd9a8cff811cbd1542462e683fe62d9d441820
Mahendran S <s.mahndran@appslabz.com>
2015-10-13 00:39:50 +0530

    Created Recyclerview with values from 1 to 100

--- app/src/main/java/test/mahendran/testing/MainActivity.java
+++ app/src/main/java/test/mahendran/testing/MainActivity.java
@@ -1,13 +1,20 @@
 package test.mahendran.testing;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout lv = new LinearLayout(this);
+        lv.setLayoutParams(layoutParams);
+        View v = new TextView(this);
+        v.setLayoutParams(layoutParams);
+        v.setId(R.id.reservedId);
+        lv.addView(v);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLinearLayoutManager);
+        AdapterRecylerView mAdapter = new AdapterRecylerView(this, lv);
+        recyclerView.setAdapter(mAdapter);
     }
 
     @Override
@@ -43,10 +63,45 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AdapterRecylerView extends RecyclerView.Adapter<AdapterRecylerView.ViewHolder>{
+        private Context context;
+        private View lv;
+
+        public AdapterRecylerView(Context context, LinearLayout lv) {
+            this.context = context;
+            this.lv = lv;
+        }
+
+        @Override
+        public AdapterRecylerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            ViewHolder vh = new ViewHolder(lv);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(AdapterRecylerView.ViewHolder holder, int position) {
+            holder.mTextView.setText(""+(position+1));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            public TextView mTextView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView.findViewById(R.id.reservedId);
+            }
+        }
     }
 }
