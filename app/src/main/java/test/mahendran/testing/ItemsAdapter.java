2a431a898d28d79b4bd7fbd5c24532cea5fcd525
Mahendran Sakkarai <mahendranmails@gmail.com>
2016-12-27 00:30:28 +0530

    Implemented Recyclerview with pagination model.

---
+++ app/src/main/java/test/mahendran/testing/ItemsAdapter.java
@@ -0,0 +1,66 @@
+package test.mahendran.testing;
+
+import android.os.Handler;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nandakumar on 12/26/2016.
+ */
+public class ItemsAdapter extends RecyclerView.Adapter {
+    private ArrayList<String> mItems = new ArrayList<>();
+
+    public ArrayList<String> getItems() {
+        return mItems;
+    }
+
+    public void updateItems(ArrayList<String> listItems) {
+        mItems.addAll(listItems);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        return new ItemHolder(inflater.inflate(android.R.layout.simple_list_item_1, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ((ItemHolder) holder).bindData(getItems().get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void setLoading() {
+        mItems.add("Loading");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                notifyDataSetChanged();
+            }
+        }, 1000);
+    }
+
+    public void removeLoading() {
+        if (mItems.size() > 0)
+            mItems.remove(mItems.size() - 1);
+    }
+
+    public void setThatsAll() {
+        mItems.add("Thats All");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                notifyDataSetChanged();
+            }
+        }, 1000);
+    }
+}
