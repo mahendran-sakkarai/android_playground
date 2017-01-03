66ac800239f3ac95e5fd98fd99f482c2c4023654
Mahendran Sakkarai <mahendranmails@gmail.com>
2017-01-03 19:28:40 +0530

    Playing with service

---
+++ app/src/main/java/test/mahendran/testing/DownloadService.java
@@ -0,0 +1,20 @@
+package test.mahendran.testing;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Udhaya Kumar on 03-01-2017.
+ */
+
+public class DownloadService extends IntentService {
+
+    public DownloadService() {
+        super("Download Service");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
