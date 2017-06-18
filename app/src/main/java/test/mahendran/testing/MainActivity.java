948d73a392df3fabeb1349101572e94b7d71854a
Mahendran Sakkarai <mahendranmails@gmail.com>
2017-06-18 11:58:12 +0530

    Tried with font in xml.

--- app/src/main/java/test/mahendran/testing/MainActivity.java
+++ app/src/main/java/test/mahendran/testing/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
+import android.text.style.TextAppearanceSpan;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        SpannableStringBuilder builder = new SpannableStringBuilder("Hello World!");
+        builder.setSpan(new TextAppearanceSpan(this, R.style.NormalStyle), 0, 2, 0);
+        builder.setSpan(new TextAppearanceSpan(this, R.style.ItalicStyle), 3, 4, 0);
+        builder.setSpan(new TextAppearanceSpan(this, R.style.BoldStyle), 5, builder.length() - 1, 0);
+        ((TextView)findViewById(R.id.tv7)).setText(builder);
     }
 
     @Override
